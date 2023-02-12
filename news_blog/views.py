940e76ea3056e60fcc1de441910818fbf3d555ef
AVTpepper <Alexander.tastad@hotmail.com>
2023-02-12 18:03:54 +0000

    Add: base template extended to each page

--- news_blog/views.py
+++ news_blog/views.py
@@ -3,25 +3,46 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 
+posts = [
+    {
+        'author': 'Alex T',
+        'title': 'News Post 1',
+        'content': 'This is my first news post',
+        'date_posted': '11th September, 2023'
+    },
+    {
+        'author': 'Evelina E',
+        'title': 'News Post 2',
+        'content': 'This is my second news post',
+        'date_posted': '3rd November, 2023'
+    },
+]
+
+
 def index(request):
-    return render(request, 'news_blog/index.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'news_blog/index.html', context)
 
 
 def login(request):
-    return render(request, 'news_blog/login.html')
+    return render(request, 'news_blog/login.html', {'title': "Login Page"})
 
 
 def sign_up(request):
-    return render(request, 'news_blog/sign_up.html')
+    return render(request, 'news_blog/sign_up.html', {'title': "Signup Page"})
 
 
 def article_view(request):
-    return render(request, 'news_blog/article_view.html')
+    return render(
+        request, 'news_blog/article_view.html', {'title': "Article View"})
 
 
 def post_creation(request):
-    return render(request, 'news_blog/post_creation.html')
+    return render(
+        request, 'news_blog/post_creation.html', {'title': "Post Creation"})
 
 
 def view_all(request):
-    return render(request, 'news_blog/view_all.html')
+    return render(request, 'news_blog/view_all.html', {'title': "All Posts"})
