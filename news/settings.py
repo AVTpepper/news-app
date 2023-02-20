250f2e55590931bdc6e9b84e76a477299311a221
AVTpepper <Alexander.tastad@hotmail.com>
2023-02-20 20:02:09 +0000

    Fix: cloudinary settings and image size

--- news/settings.py
+++ news/settings.py
@@ -158,9 +158,9 @@ LOGIN_URL = 'login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# Cloudinary settings
-cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-)
+# # Cloudinary settings
+# cloudinary.config(
+#     cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+#     api_key=os.environ.get("CLOUDINARY_API_KEY"),
+#     api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+# )
