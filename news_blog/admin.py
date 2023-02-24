82c8d63fac8eb246b89c92a2576eace51cb84a56
AVTpepper <Alexander.tastad@hotmail.com>
2023-02-24 08:57:30 +0000

    Add: register comment model

--- news_blog/admin.py
+++ news_blog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Post, Profile
+from .models import Post, Profile, Comment
 
 # Register your models here.
 
 
 admin.site.register(Post)
 admin.site.register(Profile)
+admin.site.register(Comment)
+
