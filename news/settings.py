ffea5d0984f92c9403676dbf4aebeec0de62b601
AVTpepper <Alexander.tastad@hotmail.com>
2023-02-26 19:20:23 +0000

    getting ready for deployment

--- news/settings.py
+++ news/settings.py
@@ -148,10 +148,12 @@ cloudinary.config(
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(
+    BASE_DIR, 'staticfiles'
+)
 cloudinary.api.resources.secure = True
 cloudinary.uploader.secure = True
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # MEDIA_URL = '/media/'
